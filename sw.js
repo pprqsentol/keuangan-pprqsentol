2bac21ba3e410b1a9cb83d096cf612e01a1ed87a
pprqsentol <pprqsentol@gmail.com>
2026-09-18 05:23:28 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'keuangan-pprqsentol-v5-fix-laporan-belanja-toko';
+const CACHE_NAME = 'keuangan-pprqsentol-v7-select-kolom';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 const CDN_ASSETS = [
   'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2',
